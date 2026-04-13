2a9e2baf4b259e84722436577c3f10aa9838b007
jasminepalit <jasmine.palit@gmail.com>
2026-04-13 10:37:46 -0400

    bad code

--- Sponge/src/main.cpp
+++ Sponge/src/main.cpp
@@ -3,40 +3,28 @@
 int motor1pin1 = 2;
 int motor1pin2 = 3;
 
-int motor2pin1 = 4;
-int motor2pin2 = 5;
+int enablePin = 9; // ENA
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
-  pinMode(motor2pin1,  OUTPUT);
-  pinMode(motor2pin2, OUTPUT);
-  //(Optional)
-  pinMode(9,  OUTPUT); 
-  pinMode(10, OUTPUT);
-  //(Optional)
+  pinMode(enablePin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  //Controlling speed (0  = off and 255 = max speed):     
-  //(Optional)
-  analogWrite(9, 100); //ENA  pin
-  analogWrite(10, 200); //ENB pin
-  //(Optional)
-  
-  digitalWrite(motor1pin1,  HIGH);
+
+  // Set speed
+  analogWrite(enablePin, 150); // 0–255
+
+  // Forward
+  digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
-  digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, LOW);
 
   delay(3000);
-  digitalWrite(motor1pin1,  LOW);
-  digitalWrite(motor1pin2, HIGH);
 
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
+  // Stop
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, LOW);
+
   delay(3000);
 }
